b39d1b5647713f6f0de74e724ca44a5ea5add79c
Hans Yang <hyang@servicethecit.com>
2023-07-18 14:03:18 -0700

    removed all the unnecessary files (in the clearnew branch)

--- resources/views/components/booking_tab_dispatch.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-		<div class="col">
-			<p>BlaBlaBla....</p>
-		</div>
-	</div>
--- resources/views/components/driver_tab_movement.blade.php
+++
@@ -1,7 +0,0 @@
-	<div class="row">
-		<div class="col">
-			<?php
-				echo "Movements....".$dbTable->dvr_no;
-			?>
-		</div>
-	</div>
